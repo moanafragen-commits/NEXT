c37f4e51a3d6ba47549b9e62f5d407a062028165
Moana <moanafragen@gmail.com>
2026-02-15 19:38:53 +0000

    File changes

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
